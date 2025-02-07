bd583ae1cf975a3b5bc9a900ac6553431837a2af
smthemex <138738845+smthemex@users.noreply.github.com>
2025-02-07 17:32:06 +0800

    init

---
+++ node_utils.py
@@ -0,0 +1,199 @@
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import os
+import torch
+from PIL import Image
+import numpy as np
+import cv2
+from comfy.utils import common_upscale,ProgressBar
+from huggingface_hub import hf_hub_download
+
+cur_path = os.path.dirname(os.path.abspath(__file__))
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
+
+def cv2pil(cv_image):
+    """
+    将OpenCV图像转换为PIL图像
+    :param cv_image: OpenCV图像
+    :return: PIL图像
+    """
+    # 将图像从BGR转换为RGB
+    rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+    # 使用PIL的Image.fromarray方法将NumPy数组转换为PIL图像
+    pil_image = Image.fromarray(rgb_image)
+    return pil_image
+
+def tensor_to_pil(tensor):
+    image_np = tensor.squeeze().mul(255).clamp(0, 255).byte().numpy()
+    image = Image.fromarray(image_np, mode='RGB')
+    return image
+
+def tensor2pil_list(image,width,height):
+    B,_,_,_=image.size()
+    if  B==1:
+        ref_image_list=[tensor2pil_upscale(image,width,height)]
+    else:
+        img_list = list(torch.chunk(image, chunks=B))
+        ref_image_list = [tensor2pil_upscale(img,width,height) for img in img_list]
+    return ref_image_list
+
+def tensor2pil_upscale(img_tensor, width, height):
+    samples = img_tensor.movedim(-1, 1)
+    img = common_upscale(samples, width, height, "nearest-exact", "center")
+    samples = img.movedim(1, -1)
+    img_pil = tensor_to_pil(samples)
+    return img_pil
+
+
+def tensor2cv(tensor_image,RGB2BGR=True):
+    if len(tensor_image.shape)==4:#bhwc to hwc
+        tensor_image=tensor_image.squeeze(0)
+    if tensor_image.is_cuda:
+        tensor_image = tensor_image.cpu().detach()
+    tensor_image=tensor_image.numpy()
+    #反归一化
+    maxValue=tensor_image.max()
+    tensor_image=tensor_image*255/maxValue
+    img_cv2=np.uint8(tensor_image)#32 to uint8
+    if RGB2BGR:
+        img_cv2=cv2.cvtColor(img_cv2,cv2.COLOR_RGB2BGR)
+    return img_cv2
+
+def cvargb2tensor(img):
+    assert type(img) == np.ndarray, 'the img type is {}, but ndarry expected'.format(type(img))
+    img = torch.from_numpy(img.transpose((2, 0, 1)))
+    return img.float().div(255).unsqueeze(0)  # 255也可以改为256
+
+def cv2tensor(img):
+    assert type(img) == np.ndarray, 'the img type is {}, but ndarry expected'.format(type(img))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = torch.from_numpy(img.transpose((2, 0, 1)))
+    return img.float().div(255).unsqueeze(0)  # 255也可以改为256
+
+def images_generator(img_list: list,):
+    #get img size
+    sizes = {}
+    for image_ in img_list:
+        if isinstance(image_,Image.Image):
+            count = sizes.get(image_.size, 0)
+            sizes[image_.size] = count + 1
+        elif isinstance(image_,np.ndarray):
+            count = sizes.get(image_.shape[:2][::-1], 0)
+            sizes[image_.shape[:2][::-1]] = count + 1
+        else:
+            raise "unsupport image list,must be pil or cv2!!!"
+    size = max(sizes.items(), key=lambda x: x[1])[0]
+    yield size[0], size[1]
+    
+    # any to tensor
+    def load_image(img_in):
+        if isinstance(img_in, Image.Image):
+            img_in=img_in.convert("RGB")
+            i = np.array(img_in, dtype=np.float32)
+            i = torch.from_numpy(i).div_(255)
+            if i.shape[0] != size[1] or i.shape[1] != size[0]:
+                i = torch.from_numpy(i).movedim(-1, 0).unsqueeze(0)
+                i = common_upscale(i, size[0], size[1], "lanczos", "center")
+                i = i.squeeze(0).movedim(0, -1).numpy()
+            return i
+        elif isinstance(img_in,np.ndarray):
+            i=cv2.cvtColor(img_in,cv2.COLOR_BGR2RGB).astype(np.float32)
+            i = torch.from_numpy(i).div_(255)
+            #print(i.shape)
+            return i
+        else:
+           raise "unsupport image list,must be pil,cv2 or tensor!!!"
+        
+    total_images = len(img_list)
+    processed_images = 0
+    pbar = ProgressBar(total_images)
+    images = map(load_image, img_list)
+    try:
+        prev_image = next(images)
+        while True:
+            next_image = next(images)
+            yield prev_image
+            processed_images += 1
+            pbar.update_absolute(processed_images, total_images)
+            prev_image = next_image
+    except StopIteration:
+        pass
+    if prev_image is not None:
+        yield prev_image
+
+def load_images(img_list: list,):
+    gen = images_generator(img_list)
+    (width, height) = next(gen)
+    images = torch.from_numpy(np.fromiter(gen, np.dtype((np.float32, (height, width, 3)))))
+    if len(images) == 0:
+        raise FileNotFoundError(f"No images could be loaded .")
+    return images
+
+def tensor2pil(tensor):
+    image_np = tensor.squeeze().mul(255).clamp(0, 255).byte().numpy()
+    image = Image.fromarray(image_np, mode='RGB')
+    return image
+
+def pil2narry(img):
+    narry = torch.from_numpy(np.array(img).astype(np.float32) / 255.0).unsqueeze(0)
+    return narry
+
+def equalize_lists(list1, list2):
+    """
+    比较两个列表的长度，如果不一致，则将较短的列表复制以匹配较长列表的长度。
+    
+    参数:
+    list1 (list): 第一个列表
+    list2 (list): 第二个列表
+    
+    返回:
+    tuple: 包含两个长度相等的列表的元组
+    """
+    len1 = len(list1)
+    len2 = len(list2)
+    
+    if len1 == len2:
+        pass
+    elif len1 < len2:
+        print("list1 is shorter than list2, copying list1 to match list2's length.")
+        list1.extend(list1 * ((len2 // len1) + 1))  # 复制list1以匹配list2的长度
+        list1 = list1[:len2]  # 确保长度一致
+    else:
+        print("list2 is shorter than list1, copying list2 to match list1's length.")
+        list2.extend(list2 * ((len1 // len2) + 1))  # 复制list2以匹配list1的长度
+        list2 = list2[:len1]  # 确保长度一致
+    
+    return list1, list2
+
+def file_exists(directory, filename):
+    # 构建文件的完整路径
+    file_path = os.path.join(directory, filename)
+    # 检查文件是否存在
+    return os.path.isfile(file_path)
+
+def download_weights(file_dir,repo_id,subfolder="",pt_name=""):
+    if subfolder:
+        file_path = os.path.join(file_dir,subfolder, pt_name)
+        sub_dir=os.path.join(file_dir,subfolder)
+        if not os.path.exists(sub_dir):
+            os.makedirs(sub_dir)
+        if not os.path.exists(file_path):
+            file_path = hf_hub_download(
+                repo_id=repo_id,
+                subfolder=subfolder,
+                filename=pt_name,
+                local_dir = file_dir,
+            )
+        return file_path
+    else:
+        file_path = os.path.join(file_dir, pt_name)
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        if not os.path.exists(file_path):
+            file_path = hf_hub_download(
+                repo_id=repo_id,
+                filename=pt_name,
+                local_dir=file_dir,
+            )
+        return file_path
---
+++ sonic.py
@@ -0,0 +1,230 @@
+import os
+import torch
+import torch.utils.checkpoint
+from tqdm import tqdm
+import gc
+
+from .src.utils.util import seed_everything
+from .src.dataset.test_preprocess import process_bbox
+from .src.models.base.unet_spatio_temporal_condition import  add_ip_adapters
+from .src.pipelines.pipeline_sonic import SonicPipeline
+from .src.utils.RIFE.RIFE_HDv3 import RIFEModel
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def sonic_predata(wav_enc,audio_feature,audio_len,step,audio2bucket,image_encoder,audio_pe,ref_img,clip_img,device):
+ 
+    image_embeds=image_encoder.encode_image(clip_img)["image_embeds"] #torch.Size([1, 1024])
+    image_embeds=image_embeds.clone().detach().to(device, dtype=torch.float16) #dtype需要改成可选
+
+
+    audio_prompts = []
+    last_audio_prompts = []
+    window = 3000
+    #print(audio_feature.shape) #torch.Size([80, 3000])
+    for i in range(0, audio_feature.shape[-1], window):
+        audio_prompt = wav_enc.encoder(audio_feature[:,:,i:i+window], output_hidden_states=True).hidden_states
+        last_audio_prompt = wav_enc.encoder(audio_feature[:,:,i:i+window]).last_hidden_state
+        last_audio_prompt = last_audio_prompt.unsqueeze(-2)
+        audio_prompt = torch.stack(audio_prompt, dim=2)
+        audio_prompts.append(audio_prompt)
+        last_audio_prompts.append(last_audio_prompt)
+
+    audio_prompts = torch.cat(audio_prompts, dim=1)
+    audio_prompts = audio_prompts[:,:audio_len*2]
+    audio_prompts = torch.cat([torch.zeros_like(audio_prompts[:,:4]), audio_prompts, torch.zeros_like(audio_prompts[:,:6])], 1)
+
+    last_audio_prompts = torch.cat(last_audio_prompts, dim=1)
+    last_audio_prompts = last_audio_prompts[:,:audio_len*2]
+    last_audio_prompts = torch.cat([torch.zeros_like(last_audio_prompts[:,:24]), last_audio_prompts, torch.zeros_like(last_audio_prompts[:,:26])], 1)
+
+
+    ref_tensor_list = []
+    audio_tensor_list = []
+    uncond_audio_tensor_list = []
+    motion_buckets = []
+    for i in tqdm(range(audio_len//step)):
+        audio_clip = audio_prompts[:,i*2*step:i*2*step+10].unsqueeze(0)
+        audio_clip_for_bucket = last_audio_prompts[:,i*2*step:i*2*step+50].unsqueeze(0)
+        motion_bucket = audio2bucket(audio_clip_for_bucket, image_embeds)
+        motion_bucket = motion_bucket * 16 + 16
+        motion_buckets.append(motion_bucket[0])
+
+        cond_audio_clip = audio_pe(audio_clip).squeeze(0)
+        uncond_audio_clip = audio_pe(torch.zeros_like(audio_clip)).squeeze(0)
+
+        ref_tensor_list.append(ref_img[0])
+        audio_tensor_list.append(cond_audio_clip[0])
+        uncond_audio_tensor_list.append(uncond_audio_clip[0])
+
+    return ref_tensor_list,audio_tensor_list,uncond_audio_tensor_list,motion_buckets,image_embeds
+
+
+
+def preprocess_face(face_image,face_det, expand_ratio=1.0):
+        
+        h, w = face_image.shape[:2]
+        _, _, bboxes = face_det(face_image, maxface=True)
+        face_num = len(bboxes)
+        bbox = []
+        if face_num > 0:
+            x1, y1, ww, hh = bboxes[0]
+            x2, y2 = x1 + ww, y1 + hh
+            bbox = x1, y1, x2, y2
+            bbox_s = process_bbox(bbox, expand_radio=expand_ratio, height=h, width=w)
+
+        return {
+            'face_num': face_num,
+            'crop_bbox': bbox_s,
+        }
+
+def crop_face_image(face_image,crop_bbox):
+    crop_image = face_image[crop_bbox[1]:crop_bbox[3], crop_bbox[0]:crop_bbox[2]]
+    return crop_image
+
+
+def test(
+    pipe,
+    config,
+    audio_tensor_list,
+    uncond_audio_tensor_list,
+    motion_buckets,
+    width,
+    height,
+    batch,
+    image_embeds,
+    fps,
+):
+
+    ref_img = batch['ref_img']
+    face_mask= batch['face_mask']
+    video = pipe(
+        ref_img,
+        image_embeds,
+        face_mask,
+        audio_tensor_list,
+        uncond_audio_tensor_list,
+        motion_buckets,
+        height=height,
+        width=width,
+        num_frames=len(audio_tensor_list),
+        decode_chunk_size=config.decode_chunk_size,
+        motion_bucket_scale=config.motion_bucket_scale,
+        fps=fps,
+        noise_aug_strength=config.noise_aug_strength,
+        min_guidance_scale1=config.min_appearance_guidance_scale, # 1.0,
+        max_guidance_scale1=config.max_appearance_guidance_scale,
+        min_guidance_scale2=config.audio_guidance_scale, # 1.0,
+        max_guidance_scale2=config.audio_guidance_scale,
+        overlap=config.overlap,
+        shift_offset=config.shift_offset,
+        frames_per_batch=config.n_sample_frames,
+        num_inference_steps=config.num_inference_steps,
+        i2i_noise_strength=config.i2i_noise_strength
+    ).frames
+
+
+    # Concat it with pose tensor
+    # pose_tensor = torch.stack(pose_tensor_list,1).unsqueeze(0)
+    video = (video*0.5 + 0.5).clamp(0, 1)
+    video = torch.cat([video.to(pipe.device)], dim=0).cpu()
+    #print("video shape:", video.shape)# torch.Size([1, 3, 250, 512, 512])
+    return video
+
+
+class Sonic():
+   
+    def __init__(self, 
+                 device,
+                 weight_dtype,
+                 vae,
+                 val_noise_scheduler,
+                 unet,
+                 flownet_ckpt,
+                 sonic_unet,
+                 use_interframe,
+                 ip_audio_scale,
+                 ):
+        self.use_interframe = use_interframe
+        #config.use_interframe = enable_interpolate_frame
+        add_ip_adapters(unet, [32], [ip_audio_scale])
+        sonic_dict = torch.load(sonic_unet, map_location="cpu")
+        unet.load_state_dict(sonic_dict,strict=True,)
+        del sonic_dict
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        if self.use_interframe:
+            rife = RIFEModel(device=device)
+            rife.load_model(flownet_ckpt)
+            self.rife = rife
+
+        vae.to(weight_dtype)
+        unet.to(weight_dtype)
+
+        pipe = SonicPipeline(
+            unet=unet,
+            vae=vae,
+            scheduler=val_noise_scheduler,
+        )
+        self.pipe = pipe.to(dtype=weight_dtype)
+        self.device = device
+        print('init done')
+
+
+    @torch.no_grad()
+    def process(self,
+                audio_tensor_list,
+                uncond_audio_tensor_list,
+                motion_buckets,
+                test_data,
+                config,
+                image_embeds,
+                fps,
+                inference_steps=25,
+                dynamic_scale=1.0,
+                seed=None):
+        
+        # specific parameters
+        if seed:
+            config.seed = seed
+
+        config.num_inference_steps = inference_steps
+        config.motion_bucket_scale = dynamic_scale
+        seed_everything(config.seed)
+       
+        height, width = test_data['ref_img'].shape[-2:]
+        #self.pipe.enable_model_cpu_offload #太慢，没意义
+        self.pipe.to(self.device)
+        video = test(
+            self.pipe,
+            config,
+            audio_tensor_list,
+            uncond_audio_tensor_list,
+            motion_buckets,
+            width=width,
+            height=height,
+            batch=test_data,
+            image_embeds=image_embeds,
+            fps=fps,
+            )
+
+        if self.use_interframe:
+            rife = self.rife
+            out = video.to(self.device)
+            results = []
+            video_len = out.shape[2]
+            for idx in tqdm(range(video_len-1), ncols=0):
+                I1 = out[:, :, idx]
+                I2 = out[:, :, idx+1]
+                middle = rife.inference(I1, I2).clamp(0, 1).detach()
+                results.append(out[:, :, idx])
+                results.append(middle)
+            results.append(out[:, :, video_len-1])
+            video = torch.stack(results, 2).cpu()
+         
+        return video
+        
